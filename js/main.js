69b0f985f80654243e08378c4cb8ca4bf0308c6e
Alamin-hossen <hossena863@gmail.com>
2021-02-07 23:27:04 +0600

    loading time fixed

--- js/main.js
+++ js/main.js
@@ -313,7 +313,7 @@ window.addEventListener("load", () =>{
     document.querySelector(".preloader").classList.add("fade-out");
     setTimeout(() =>{
         document.querySelector(".preloader").style.display="none";
-    },900)
+    },300)
 })
 
 
